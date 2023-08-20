b4e4fa5b258eacc8e97129ba5bc90d308d75d7b1
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-20 10:17:42 -0800

    Initial

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float n1,n2;
+	char op;
+	cout<<" Basic Calculator "<<endl;
+	cout<<" Enter two numbers."<<endl;
+	cout<<" 1st Number: ";
+	cin>>n1;
+	cout<<" 2nd Number: ";
+	cin>>n2;
+	cout<<" Enter Operator: Division(/), Multiplication(*), Addition(+), Subtraction(-)"<<endl;
+	cin>>op;
+	return 0;
+}
