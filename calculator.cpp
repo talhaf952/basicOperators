3debf73bb81ab8fdee93eb26e98a48e36e18a65e
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-20 10:23:48 -0800

    Final

--- calculator.cpp
+++ calculator.cpp
@@ -10,7 +10,28 @@ int main()
 	cin>>n1;
 	cout<<" 2nd Number: ";
 	cin>>n2;
-	cout<<" Enter Operator: Division(/), Multiplication(*), Addition(+), Subtraction(-)"<<endl;
+	cout<<" Enter Operator: Division(/), Multiplication(*), Addition(+), Subtraction(-) : ";
 	cin>>op;
+	switch (op)
+	{
+		case'/':
+		cout<<" "<<n1/n2;
+		break;
+		
+		case'*':
+		cout<<" "<<n1*n2;
+		break;
+		
+		case'+':
+		cout<<" "<<n1+n2;
+		break;
+		
+		case'-':
+		cout<<" "<<n1-n2;
+		break;
+		
+		default:
+			cout<<" "<<"Error! Choose correct operator.";
+	}
 	return 0;
 }
